b6f41b83db4ccd1658741f437acf397612731c3d
05113 <776080527@qq.com>
2019-10-07 17:09:01 +0800

    third

--- src/main/java/com/quicktron/wcsmock/MailService.java
+++ src/main/java/com/quicktron/wcsmock/MailService.java
@@ -4,4 +4,5 @@ package com.quicktron.wcsmock;
 public interface MailService {
     public void sendMail(String title, String url, String email);
     public void test();
+    public void test1();
 }
